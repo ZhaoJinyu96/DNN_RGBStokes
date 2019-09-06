da81e692b25caf81fa5a19ffd34007841f2ae40c
Jinyu Zhao <51068834+ZhaoJinyu96@users.noreply.github.com>
2019-09-06 09:41:37 +0900

    Update visualize_loss.py

--- visualizers/visualize_loss.py
+++ visualizers/visualize_loss.py
@@ -5,13 +5,38 @@ Created on Thu Jan 10 11:38:56 2019
 @author: 0000145046
 """
 
-import sys
+"""import sys
 sys.path.append("../modules/")
 
 import yaml
 import matplotlib.pyplot as plt
 
-import my_matplotlib_funcs as myplt
+import my_matplotlib_funcs as myplt"""
+
+import sys
+sys.path.append("../")
+
+import yaml
+import pathlib
+
+import numpy as np
+import cv2
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib import gridspec
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+from mymodules.myutils.pathutils import find_same_id
+from mymodules.myutils.pathutils import find_same_id_s0
+from mymodules.myutils.pathutils import extractID
+from mymodules.myutils.pathutils import  returnGTPaths
+from mymodules.myutils.pathutils import  returnRecPaths
+from mymodules.myutils.pathutils import  returnS0Paths
+import mymodules.myutils.imageutils as nm
+from mymodules.myutils.imageutils import MAX_16BIT
+import mymodules.myutils.mplutils as myplt
+
 
 #----------------------
 # define file names
@@ -62,7 +87,8 @@ ax.plot(x, train_loss, label="train")
 ax.set_yscale("log")
 ax.grid(which="both")
 ax.set_title("loss on each epoch", fontsize=18)
-ax.set_ylim(10**0, 10**3)
+#ax.set_ylim(10**0, 10**3)
+ax.set_ylim(10**0, 10**4)
 ax.set_xlabel("epoch number", fontsize=15)
 ax.set_ylabel("value of loss", fontsize=15)
 ax.legend(fontsize=12)
